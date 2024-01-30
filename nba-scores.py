08592dc18565e4d04cc4fcfa4f0dcd57fba99935
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-30 10:50:03 -0300

    get data

--- nba-scores.py
+++ nba-scores.py
@@ -3,3 +3,8 @@ from pprint import PrettyPrinter
 
 BASE_URL = "https://data.nba.net"
 ALL_JSON = "/prod/v1/today.json"
+
+printer = PrettyPrinter()
+
+data = get(BASE_URL + ALL_JSON).json()
+printer.pprint(data)
